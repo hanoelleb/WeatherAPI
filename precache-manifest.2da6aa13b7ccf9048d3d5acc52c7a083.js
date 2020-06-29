a032d055c1525972ab79a30ea2299f7515468431
Hannah <hanoelleb@gmail.com>
2020-06-28 23:12:06 -0700

    Updates

---
+++ precache-manifest.2da6aa13b7ccf9048d3d5acc52c7a083.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3058724f0e4bb2ac3e9de210bf6352c7",
+    "url": "/WeatherApp/index.html"
+  },
+  {
+    "revision": "685eeb3c34182bac9d86",
+    "url": "/WeatherApp/static/css/main.f4742dda.chunk.css"
+  },
+  {
+    "revision": "ee00750033afdcee255b",
+    "url": "/WeatherApp/static/js/2.d4e5d965.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WeatherApp/static/js/2.d4e5d965.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "685eeb3c34182bac9d86",
+    "url": "/WeatherApp/static/js/main.5c022e5b.chunk.js"
+  },
+  {
+    "revision": "7577014ea32ae9893346",
+    "url": "/WeatherApp/static/js/runtime-main.50b632fe.js"
+  }
+]);
